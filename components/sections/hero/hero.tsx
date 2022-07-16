e951e3124e4d400b435668053102a0ccce8c91df
vanderms <vanderms.84@gmail.com>
2022-07-16 19:43:48 -0300

    implemented hero section

--- components/sections/hero/hero.tsx
+++ components/sections/hero/hero.tsx
@@ -2,21 +2,21 @@ import Link from "next/link";
 
 export const HeroSection: React.FC = () => {
   return (
-    <section className={`container mt-16 grid grid-cols-1 gap-[3.75rem]`}>
-      <div className="max-w-[35rem] mx-auto">
-        <div className="vn__hero-image"></div>
-        <header>
+    <section className={`container mt-4 lg:mt-[4.875rem]`}>
+      <div className="max-w-[35rem] mx-auto grid grid-cols-1 gap-[3.75rem] lg:grid-cols-[35.25rem,1fr] lg:gap-[6.25rem] lg:max-w-full">
+        <div className="vn__hero-image lg:order-1"></div>
+        <header className="lg:mt-[3.8125rem]">
           <h1 className="vn__sm-title-xl lg:vn__lg-title-xl">
             More than just shorter links
           </h1>
-          <p className="mt-4 vn__sm-text-xl lg:vn__lg-text-xl">
+          <p className="mt-4 vn__sm-text-xl lg:vn__lg-text-xl lg:mt-1">
             Build your brand&apos;s recognition and get detailed insights on how your links
             are performing.
           </p>
           <Link href="/">
             <a
               className={`grid place-items-center text-white text-xl w-max h-14 px-10
-             bg-primary rounded-full hover:bg-primary-hover mt-8 mx-auto`}
+             bg-primary rounded-full hover:bg-primary-hover mt-8 mx-auto lg:mx-0 lg:mt-10`}
             >
               Get Started
             </a>
--- pages/index.tsx
+++ pages/index.tsx
@@ -11,20 +11,22 @@ const Home: NextPage = () => {
 
   return (
     <>
-      <Head>
-        <meta property="og:site_name" content={name} />
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={description} />
-        <meta property="og:image" content={url + "assets/preview.jpg"} />
-        <meta property="og:type" content="website" />
-        <meta property="og:image:type" content="image/jpeg" />
-        <meta property="og:url" content={url}></meta>
-        <title>{title}</title>
-      </Head>
-      <Navbar />
-      <main>
-        <HeroSection/>
-      </main>
+      <div className="container-full">
+        <Head>
+          <meta property="og:site_name" content={name} />
+          <meta property="og:title" content={title} />
+          <meta property="og:description" content={description} />
+          <meta property="og:image" content={url + "assets/preview.jpg"} />
+          <meta property="og:type" content="website" />
+          <meta property="og:image:type" content="image/jpeg" />
+          <meta property="og:url" content={url}></meta>
+          <title>{title}</title>
+        </Head>
+        <Navbar />
+        <main>
+          <HeroSection />
+        </main>
+      </div>
     </>
   );
 };
