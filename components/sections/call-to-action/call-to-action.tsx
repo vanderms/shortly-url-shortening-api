d0d4fd378f67c2b5d9829d1ddf3e49ff6c417fce
vanderms <vanderms.84@gmail.com>
2022-07-18 14:15:45 -0300

    update readme

--- components/sections/call-to-action/call-to-action.tsx
+++ components/sections/call-to-action/call-to-action.tsx
@@ -5,9 +5,12 @@ export const CallToActionSection: React.FC = () => {
   const titleId = useId();
 
   return (
-    <section aria-labelledby={titleId} className="bg-bgdark py-[5.625rem] vn__boost-pattern relative lg:py-[3.5625rem]">
+    <section
+      aria-labelledby={titleId}
+      className="bg-bgdark py-[5.625rem] vn__boost-pattern relative lg:py-[3.5625rem]"
+    >
       <div className="container flex flex-col gap-4 items-center relative z-10 lg:gap-8">
-        <h2 className="vn__sm-title-md lg:vn__lg-title-md !text-white">
+        <h2 className="vn__sm-title-md lg:vn__lg-title-md !text-white" id={titleId}>
           Boost your links today
         </h2>
         <div>
